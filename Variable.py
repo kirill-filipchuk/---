050f3d3bea5b8e023ab3c234dbc96cac0d543db4
kirill-filipchuk <162884900+kirill-filipchuk@users.noreply.github.com>
2024-07-26 16:18:09 +0500

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,5 @@
+Homework_done = 12 # выполненые ДЗ
+Time = 1.5 # затраченное время в часах
+Title ="Python" # название
+Average = (Time / Homework_done) # среднее время на одно задание
+print(Title,", всего задач:",Homework_done ,"затрачено часов:" , Time , "среднее время выполнения", Average, "часа")
