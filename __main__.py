fb1c48c498edddda7794f975491175dabc553dad
kirill-filipchuk <162884900+kirill-filipchuk@users.noreply.github.com>
2024-07-26 12:35:39 +0500

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,8 @@
+#"1st program"
+print((9 ** 0.5) * 5)
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+#"3rd program"
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+#"4th progeram"
+print(13.42 % 42.13 == 13.42 or 42.13 == 42.13 % 13.42)
