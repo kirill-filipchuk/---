bc954edb05f8e7dbd7102f6689a1c12ffc030dfe
kirill-filipchuk <162884900+kirill-filipchuk@users.noreply.github.com>
2024-07-29 21:52:10 +0500

    Add files via upload

---
+++ Organization_of_programs(homework4).py
@@ -0,0 +1,7 @@
+my_string = input("Введите ваш текст: ")
+print(my_string) # изначальный текст
+print(my_string .upper()) # текст в верхнем регистре
+print(my_string .lower()) # текст в нижнем регистре
+print(my_string .replace(" ","")) # текст без пробелов
+print(my_string[0]) # первый символ текста
+print(my_string[-1]) # последний символ текста
