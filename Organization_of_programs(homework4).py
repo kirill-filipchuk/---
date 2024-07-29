ae20d339d8481732d76673fa402f81120b1d06e1
kirill-filipchuk <162884900+kirill-filipchuk@users.noreply.github.com>
2024-07-29 21:51:17 +0500

    Delete Organization_of_programs(homework4).py

--- Organization_of_programs(homework4).py
+++
@@ -1,7 +0,0 @@
-my_string = input()
-print(my_string) # изначальный текст
-print(my_string .upper()) # текст в верхнем регистре
-print(my_string .lower()) # текст в нижнем регистре
-print(my_string .replace(" ","")) # текст без пробелов
-print(my_string[0]) # первый символ текста
-print(my_string[-1]) # последний символ текста
