728ed3fbe61752fafdcd185521cb4535065f4a53
kirill-filipchuk <162884900+kirill-filipchuk@users.noreply.github.com>
2024-08-03 18:50:36 +0500

    Update Organization_of_programs(homework4).py

--- Organization_of_programs(homework4).py
+++ Organization_of_programs(homework4).py
@@ -1,5 +1,6 @@
 my_string = input("Введите ваш текст: ")
-print(my_string) # изначальный текст
+print(my_string) #изначальный текст
+print(len(my_string)) # кол-во символов
 print(my_string .upper()) # текст в верхнем регистре
 print(my_string .lower()) # текст в нижнем регистре
 print(my_string .replace(" ","")) # текст без пробелов
